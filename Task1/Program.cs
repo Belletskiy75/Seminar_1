bd9ac1ee5ace4d855ebb9074b1f3dd1b7bfc6067
alex belle <belletskiy75@mail.com>
2024-02-24 22:15:43 +0300

    внесли изменения

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.Write("num1: ");
-string var1 = Console.ReadLine();
+string var1 = Console.ReadLine()!;
 int var_num1 = int.Parse(var1);
 Console.Write("num2: ");
-string var2 = Console.ReadLine();
+string var2 = Console.ReadLine()!;
 int var_num2 = int.Parse(var2);
 if (var_num1 == var_num2 * var_num2)
     Console.WriteLine("Да");
